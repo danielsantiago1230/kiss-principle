1258241b13d62e04bdefa159b17a4b9a0d588202
Santiago <danielsantiago1230@gmail.com>
2024-01-27 12:47:41 -0500

    set up firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import {app} from '../src/firebase/firebase'
 
 function App() {
+  console.log(app);
   return (
     <div className="App">
       <AppRouter />
