4a1257f89b938eb4860a07286d6b0fa5970f622d
Santiago <danielsantiago1230@gmail.com>
2024-01-27 07:20:09 -0500

    design home page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+// styles
+import '../styles/global.css';
 
 const Contact = () => {
     return (
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div style={styles.container}>
+            <div style={styles.circle}>
+                <h1 style={styles.heading}>Kiss Principle</h1>
+                <p style={styles.paragraph}>This is a React Web app, a reflection of my personal brand</p>
+            </div>
         </div>
     );
 };
+
+const styles: { [key: string]: CSSProperties } = {
+    container: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        // color: "#2F4159",
+    },
+    heading: {
+        textAlign: "center",
+        fontWeight: 100,
+        fontSize: "2.8rem",
+        marginBottom: "0"
+    },
+    paragraph: {
+        marginTop: "1rem",
+        textAlign: "center",
+        fontWeight: 100,
+        fontSize: "1.25rem",
+        margin: "1.25rem",
+    }
+}
 
 export default Home;
