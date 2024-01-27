b91ed6cd4eb2d7117599873c3ecd2931d8cde845
Santiago <danielsantiago1230@gmail.com>
2024-01-27 13:27:13 -0500

    firebase is working remove the log

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import AppRouter from './AppRouter';
-import {app} from '../src/firebase/firebase'
 
 function App() {
-  console.log(app);
   return (
     <div className="App">
       <AppRouter />
